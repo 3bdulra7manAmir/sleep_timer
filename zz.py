ee0c5961116af336763e4edea21576e7b55bd293
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-06-19 17:14:23 +0300

    first commit

---
+++ zz.py
@@ -0,0 +1,58 @@
+#import mouse
+#import pyautogui
+import time
+import os
+
+
+sleep_time = 900 #time in Seconds #900 = 15 minutes
+print("sleep started")
+time.sleep(sleep_time)
+
+os.system("shutdown /s /t 1")
+
+# x_ej_ico = 1518
+# y_ej_ico = 1045
+
+# pyautogui.moveTo(x_ej_ico, y_ej_ico, duration= 0.2)
+# time.sleep(0.5)
+# mouse.click("left")
+# time.sleep(0.5)
+
+# x_ej_actual = 1635
+# y_ej_actual = 1000
+
+# pyautogui.moveTo(x_ej_actual, y_ej_actual, duration= 0.2)
+# mouse.click("left")
+# time.sleep(30)
+
+# x_ej_ico = 1518
+# y_ej_ico = 1045
+
+# pyautogui.moveTo(x_ej_ico, y_ej_ico, duration= 0.2)
+# time.sleep(0.5)
+# mouse.click("left")
+# time.sleep(0.5)
+
+# x_ej_actual = 1635
+# y_ej_actual = 1000
+
+# pyautogui.moveTo(x_ej_actual, y_ej_actual, duration= 0.2)
+# mouse.click("left")
+# time.sleep(10)
+
+# x_start_menu = 37
+# y_start_menu = 1045
+# pyautogui.moveTo(x_start_menu, y_start_menu, duration= 0.2)
+# mouse.click("left")
+# time.sleep(1)
+
+# x_pow_off = 727
+# y_pow_off = 958
+
+# pyautogui.moveTo(x_pow_off, y_pow_off, duration= 0.2)
+# mouse.click("left")
+# time.sleep(1)
+
+# x_sht_dwn = 727
+# y_sht_dwn = 865
+# mouse.click("left")
